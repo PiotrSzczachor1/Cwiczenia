5c0d9a0c75424c4afb069584e62aa353e2d044d8
PiotrSzczachor1 <piotr.czerek@wp.pl>
2020-10-15 11:44:25 +0200

    Added while.

---
+++ Programy/while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int licznik;
+    
+    while(licznik <= 10)
+    {
+        printf("%d Petle sa okej\n", licznik);
+        licznik++;
+    }
+    return 0;
+}
+        
