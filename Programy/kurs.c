73a48aa977473ef90de773ebc33cfa6f63d8fc29
Piotr Szczachor <piotr.szczachor1@wp.pl>
2020-10-15 11:15:47 +0200

    first commit

---
+++ Programy/kurs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int litera;
+    for(litera='A'; litera<='Z'; litera++)
+    {
+    printf("%c\n", litera);
+    
+    }
+        return 0;
+}
