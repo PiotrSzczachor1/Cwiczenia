ed5f594fe1e21be79f7ac1731da32ad8893e3e45
PiotrSzczachor1 <72915454+PiotrSzczachor1@users.noreply.github.com>
2020-10-23 12:52:25 +0200

    Add files via upload

---
+++ Cwiczenia/WyszukiwanieZnakuTablicy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    char a, tab[100]="342550";
+    int i=0;
+    scanf("%c", &a);
+    while(!tab[i]==NULL)
+    {
+        if(tab[i]==a)
+        {
+            printf("Podany znak znajduje sie w tablicy\n");
+            return 0;
+        }
+
+        i++;
+
+    }
+    printf("Podany znak nie znajduje sie tablicy");
+    return 0;
+
+}
