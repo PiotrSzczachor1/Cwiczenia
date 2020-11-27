5700b635a4eadc71d6df48310490f28f136dd359
PiotrSzczachor1 <72915454+PiotrSzczachor1@users.noreply.github.com>
2020-11-27 01:07:42 +0100

    Chess Update

---
+++ Chess/convert_number_to_letter.h
@@ -0,0 +1,37 @@
+char convert_number_to_letter( int c)
+{
+    char kolumna;
+    if(c==0)
+    {
+        kolumna='A';
+    }
+    if(c==1)
+    {
+        kolumna='B';
+    }
+    if(c==2)
+    {
+        kolumna='C';
+    }
+    if(c==3)
+    {
+        kolumna='D';
+    }
+    if(c==4)
+    {
+        kolumna='E';
+    }
+    if(c==5)
+    {
+        kolumna='F';
+    }
+    if(c==6)
+    {
+        kolumna='G';
+    }
+    if(c==7)
+    {
+        kolumna='H';
+    }
+    return kolumna;
+}
--- Chess/move.h
+++ Chess/move.h
@@ -1,3 +1,6 @@
+#include "convert_letter_to_number.h"
+#include "convert_number_to_letter.h"
+
 void move(int *licznik, char board[8][8], int hidden_board[8][8])
 {
     int b,c;
@@ -8,84 +11,29 @@ void move(int *licznik, char board[8][8], int hidden_board[8][8])
         printf("Ruch koloru bialego: \n");
         printf("Wybierz wspolrzedna figury ktora chcesz sie ruszyc np. A5: ");
         scanf(" %c %d", &a, &b);
-        if(a=='A'||a=='a')
-        {
-            c=0;
-        }
-        if(a=='B'||a=='b')
-        {
-            c=1;
-        }
-        if(a=='C'||a=='c')
-        {
-            c=2;
-        }
-        if(a=='D'||a=='d')
-        {
-            c=3;
-        }
-        if(a=='E'||a=='e')
-        {
-            c=4;
-        }
-        if(a=='F'||a=='f')
-        {
-            c=5;
-        }
-        if(a=='G'||a=='g')
-        {
-            c=6;
-        }
-        if(a=='H'||a=='h')
-        {
-            c=7;
-        }
+        c=convert_letter_to_number(a);
         if(board[b-1][c]=='W') //Ruchy wiezy
         {
             int d=c;
             printf("Mozliwe ruchy: \n");
-            while(hidden_board[b-1][c+1]!=2&&c<8)
+            while(hidden_board[b-1][c+1]!=2&&c<7) //Dopoki wieza idac w prawo nie wyjdzie poza plansze lub nie napotka figury swojego koloru wypisuj mozliwosci ruchu wiezy w prawo
             {
-                if(c==0)
-                {
-                    kolumna='A';
-                }
-                if(c==1)
-                {
-                    kolumna='B';
-                }
-                if(c==2)
-                {
-                    kolumna='C';
-                }
-                if(c==3)
-                {
-                    kolumna='D';
-                }
-                if(c==4)
-                {
-                    kolumna='E';
-                }
-                if(c==5)
-                {
-                    kolumna='F';
-                }
-                if(c==6)
-                {
-                    kolumna='G';
-                }
-                if(c==7)
-                {
-                    kolumna='H';
-                }
+                kolumna=convert_number_to_letter(c);
                 printf("%c%d ", kolumna+1, b);
                 c++;
             }
-            c=d;
-            while((hidden_board[b-1][c-1]!=2||hidden_board[b-1][c-1]!=1)&&c>0)
+            c=d; //Przywracamy wartosc wpisana przez uzytkownika do c
+            while(hidden_board[b-1][c-1]!=2&&c>0)
             {
-                printf("%d%d ", c, b);
+                kolumna=convert_number_to_letter(c);
+                printf("%c%d ", kolumna-1, b);
                 c--;
+            }
+            c=d; //Przywracamy wartosc wpisana przez uzytkownika do c
+            while(hidden_board[b-1][c-1]!=2&&b<8)
+            {
+                kolumna=convert_number_to_letter(c)
+                printf("%c%d") //Wroc do wypisywania pozycji w gore
             }
 
         }
