a5c5bd52a443c51ce038dcf8b4ecde8a47d1c561
PiotrSzczachor1 <72915454+PiotrSzczachor1@users.noreply.github.com>
2020-11-28 03:34:30 +0100

    Added knight_possible moves

--- Chess/knight_possible_moves.h
+++ Chess/knight_possible_moves.h
@@ -5,42 +5,110 @@ void knight_possible_moves(int hidden_board[8][8], char board[8][8], int b, int 
             char kolumna; //c - kolumna, b - wiersz
             int d=c, e=b;
             printf("Mozliwe ruchy: \n");
-            printf("W prawo: \n");
+            printf("W prawo, gora: ");
             kolumna=convert_number_to_letter(c);
 
-            if( ( (b-3)>=0 )&&( (c+1)<8 )&&( hidden_board[b+1][c+1]!=color ) ) //2 w gore jeden w prawo
+            if( ( (b-3)>=0 )&&( (c+1)<8 )&&( hidden_board[b-3][c+1]!=color ) ) //2 w gore jeden w prawo
             {
                 if(hidden_board[b-3][c+1]==enemy_color) //2 w gore jeden w prawo
                 {
-                    printf("Mozliwe bicie: %c%d\n", kolumna+1, b-2); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                    printf("Mozliwe bicie: %c%d ", kolumna+1, b-2); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
                 }
                 else
                 {
-                    printf("%c%d\n",kolumna+1, b-2);
+                    printf("%c%d ",kolumna+1, b-2);
                 }
             }
-            if( ( (b-2)>=0 )&&( (c+2)<8 )&&( hidden_board[b+1][c+1]!=color ) ) //1 w gore 2 w prawo
+            if( ( (b-2)>=0 )&&( (c+2)<8 )&&( hidden_board[b-2][c+2]!=color ) ) //1 w gore 2 w prawo
             {
                 if(hidden_board[b-2][c+2]==enemy_color) //1 w gore 2 w prawo
                 {
-                    printf("Mozliwe bicie: %c%d\n", kolumna+2, b-1); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                    printf("Mozliwe bicie: %c%d ", kolumna+2, b-1); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
                 }
                 else
                 {
-                    printf("%c%d\n",kolumna+1, b-2);
+                    printf("%c%d ",kolumna+2, b-1);
                 }
             }
-            if( ( (b)<7 )&&( (c+1)<8 )&&( hidden_board[b+1][c+1]!=color ) ) //1 w dol 2 w prawo
+            printf("\n");
+            printf("W prawo, dol: ");
+            if( ( b<8 )&&( (c+2)<8 )&&( hidden_board[b][c+2]!=color ) ) //1 w dol 2 w prawo
             {
                 if(hidden_board[b][c+2]==enemy_color) // 1 w dol 2 w prawo
                 {
-                    printf("Mozliwe bicie: %c%d\n", kolumna+2, b); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                    printf("Mozliwe bicie: %c%d ", kolumna+2, b+1); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
                 }
                 else
                 {
-                    printf("%c%d\n",kolumna+2, b);
+                    printf("%c%d ",kolumna+2, b+1);
+                }
+
+            }
+            if( ( (b+1)<8)&&( (c+1)<8)&&(hidden_board[b+1][c+1]!=color) ) //2 w col 1 w prawo
+            {
+                if(hidden_board[b+1][c+1]==enemy_color)
+                {
+                    printf("Mozliwe bicie: %c%d ", kolumna+1, b+2); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                }
+                else
+                {
+                    printf("%c%d ",kolumna+1, b+2);
+                }
+
+            }
+            printf("\n");
+            printf("w lewo, gora: ");
+            if( ( (b-3)>=0 )&&( (c-1)>=0 )&&( hidden_board[b-3][c-1]!=color ) ) //2 w gore jeden w lewo
+            {
+                if(hidden_board[b-3][c-1]==enemy_color) //2 w gore jeden w prawo
+                {
+                    printf("Mozliwe bicie: %c%d ", kolumna-1, b-2); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                }
+                else
+                {
+                    printf("%c%d ",kolumna-1, b-2);
                 }
             }
+            if( ( (b-2)>=0 )&&( (c-2)>=0 )&&( hidden_board[b-2][c-2]!=color ) ) //1 w gore 2 w lewo
+            {
+                if(hidden_board[b-2][c-2]==enemy_color) //1 w gore 2 w lewo
+                {
+                    printf("Mozliwe bicie: %c%d ", kolumna-2, b-1); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                }
+                else
+                {
+                    printf("%c%d ",kolumna-2, b-1);
+                }
+            }
+            printf("\n");
+            printf("W prawo, dol: ");
+            if( ( b<8 )&&( (c-2)>=0 )&&( hidden_board[b][c-2]!=color ) ) //1 w dol 2 w lewo
+            {
+                if(hidden_board[b][c-2]==enemy_color) // 1 w dol 2 w lewo
+                {
+                    printf("Mozliwe bicie: %c%d ", kolumna-2, b+1); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                }
+                else
+                {
+                    printf("%c%d ",kolumna-2, b+1);
+                }
+
+            }
+            if( ( (b+1)<8)&&( (c-1)>=0)&&(hidden_board[b+1][c-1]!=color) ) //2 w col 1 w lewo
+            {
+                if(hidden_board[b+1][c-1]==enemy_color)
+                {
+                    printf("Mozliwe bicie: %c%d ", kolumna-1, b+2); //Jezeli na tym polu jest pionek przeciwnika daj informacje o biciu
+                }
+                else
+                {
+                    printf("%c%d ",kolumna-1, b+2);
+                }
+
+            }
+            printf("\n");
+
+
         }
 
 }
--- Chess/move.h
+++ Chess/move.h
@@ -23,6 +23,7 @@ void move(int *licznik, char board[8][8], int hidden_board[8][8])
         scanf(" %c %d", &a, &b);
         c=convert_letter_to_number(a);
         rook_possible_moves(hidden_board, board, b, c, 1, 2); //ukryta plansza cyfr, plansza widoczna, wiersz, kolumna, kolor(2 - white, 1- black), kolor przeciwnika(2 - white, 1 - black)
+        knight_possible_moves(hidden_board, board, b, c, 1, 2);
     }
     *licznik=*licznik+1;
 }
